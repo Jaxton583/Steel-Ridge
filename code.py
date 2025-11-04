fe0808e6dccdc8bc969be63236c86591b4c66fd3
Jaxton <235787475+Jaxton583@users.noreply.github.com>
2025-11-03 17:08:15 -0800

    hi world

--- code.py
+++
@@ -1,1 +0,0 @@
-print("marcus is the goat fr fr f rf r fr fr rf rf fr fr  rf rf")
--- code_2.py
+++
@@ -1,3 +0,0 @@
-print("marcus is the goat fr fr f rf r fr fr rf rf fr fr  rf rf")
-
-name = input("press enter to exit")
